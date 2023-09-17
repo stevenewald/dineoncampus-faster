5a1129b346f51703bc66c4dc87570ba7e93f5884
stevenewald <stevenewald6@gmail.com>
2023-09-17 13:47:59 +0100

    Refactored to texting folder

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 curl -L https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz > geckodriver.tar.gz
 tar -xf geckodriver.tar.gz
 rm geckodriver.tar.gz
+python3 -m pip install selenium
